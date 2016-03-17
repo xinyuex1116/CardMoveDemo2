877c8e49b67727bca10c2960488a9dbd6475fba0
xinyuex1116 <xinyuex@yeah.net>
2016-03-17 17:27:13 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jay/cardmovedemo2/Card.java
@@ -0,0 +1,113 @@
+package com.example.jay.cardmovedemo2;
+
+import android.graphics.Paint;
+import android.support.annotation.DrawableRes;
+import android.widget.ImageView;
+
+import java.util.Collection;
+
+/**
+ * Created by jay on 16/3/8.
+ */
+public class Card {
+
+    public enum Suit{Spade, Club, Heart, Diamond}
+
+    Pile pile;
+    ImageView img;
+    private int rank;
+    private Suit suit;
+    private int color;
+    private boolean isOpen = false;
+    @DrawableRes int imgId;
+
+    public Card(Pile pile, ImageView img, int rank, Suit suit, @DrawableRes int imgId){
+        this.img = img;
+        pile.addCard(this);
+        this.rank = rank;
+        this.suit = suit;
+        this.imgId = imgId;
+
+        switch (suit){
+            case Spade:
+            case Club:
+                color = 0;
+                break;
+            case Heart:
+            case Diamond:
+                color = 1;
+                break;
+
+        }
+
+        if(isOpen) {
+            img.setImageResource(imgId);
+        }
+        else {
+            img.setImageResource(R.drawable.back);
+        }
+    }
+
+    public int getRank(){
+        return rank;
+    }
+
+    public Suit getSuit(){
+        return suit;
+    }
+
+    public boolean isOpen(){
+        return isOpen;
+    }
+
+    public void openCard(){
+        isOpen = true;
+        img.setImageResource(imgId);
+    }
+
+    public void closeCard(){
+        isOpen = false;
+        img.setImageResource(R.drawable.back);
+    }
+
+    public void setLocation(float x, float y){
+        this.img.setX(x);
+        this.img.setY(y);
+    }
+
+    public boolean isAlternatingCard(Card compareCard){
+        if(this.color+compareCard.color == 1){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean isSameSuit(Card compareCard){
+        if(this.suit == compareCard.suit){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean isOneLower(Card compareCard){
+        if(compareCard.rank - this.rank == 1){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean isOneHigher(Card compareCard){
+        if(this.rank - compareCard.rank == 1){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/jay/cardmovedemo2/FoundationPile.java
@@ -0,0 +1,32 @@
+package com.example.jay.cardmovedemo2;
+
+import android.graphics.Point;
+import android.graphics.Rect;
+
+import java.util.AbstractCollection;
+import java.util.ArrayList;
+
+/**
+ * Created by jay on 16/3/15.
+ */
+public class FoundationPile {
+    ArrayList<Card> list = new ArrayList<Card>();
+    Rect area;
+    int width = 93;
+    int height = 143;
+    Card.Suit suit;
+
+    public FoundationPile(Point point, Card.Suit suit){
+        area = new Rect(point.x, point.y, point.x + width, point.y + height);
+        this.suit = suit;
+    }
+
+    public void addCard(ArrayList<Card> tempList){
+        if(tempList.size()==1) {
+            Card card = tempList.get(0);
+            if (card.getRank() == 1 && card.getSuit() == suit) {
+                list.add(card);
+            }
+        }
+    }
+}
